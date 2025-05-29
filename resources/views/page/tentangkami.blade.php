35960477fe4778bf38ad9d6d7830e3055a85d72a
Fodly <alonamasaya@gmail.com>
2025-05-30 03:07:19 +0700

    ui responsive

--- resources/views/page/tentangkami.blade.php
+++ resources/views/page/tentangkami.blade.php
@@ -13,7 +13,7 @@
 
     <!-- Logo dan Deskripsi -->
     <section class="about-section">
-        <img src="{{ asset('img/logo.png') }}" alt="Logo UMKM" class="about-logo">
+        <img src="{{ asset('img/logo/logo.png') }}" alt="Logo UMKM" class="about-logo">
         <p class="about-desc">
             Kami adalah UMKM yang berdedikasi untuk menghadirkan produk terbaik kepada pelanggan. Dengan komitmen terhadap kualitas dan pelayanan, kami terus berkembang dan berinovasi.
         </p>
@@ -34,8 +34,8 @@
     <!-- Lokasi Toko -->
     <section class="lokasi-section">
         <h2>Lokasi Kami</h2>
-        <p>Jl. Contoh Alamat No. 123, Kota Contoh, Indonesia</p>
-        <a href="https://maps.google.com/?q=-6.200000,106.816666" target="_blank" class="btn-maps">Buka di Google Maps</a>
+        <p>JL. pengarang, tengah, jambesari, Kab Bondowoso, Jawa Timur 68264</p>
+        <a href="https://www.google.com/maps/place/Mie+ayam+solo+bondowoso/@-7.9792516,113.8646762,14.25z/data=!4m6!3m5!1s0x2dd6c3d801158829:0x9e8826c174b49f53!8m2!3d-7.9783939!4d113.85961!16s%2Fg%2F11wnhtz__c?entry=ttu&g_ep=EgoyMDI1MDUyNy4wIKXMDSoASAFQAw%3D%3D" target="_blank" class="btn-maps">Buka di Google Maps</a>
     </section>
 </section>
 @endsection
